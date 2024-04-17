96448b47bbec1c2e257baf9dd17661daaa75024d
Kollan House <kollanh@gmail.com>
2024-04-17 15:52:58 +0800

    feat: adds finalize proposal function

--- lib/client/rpc/proposals.ts
+++ lib/client/rpc/proposals.ts
@@ -4,6 +4,7 @@ import { ProposalWithVaults } from "../../types/proposals";
 import { FutarchyProposalsClient } from "../client";
 import { ConditionalVault } from "../../idl/conditional_vault";
 import { VaultAccount } from "../../types/conditionalVault";
+import { PublicKey } from "@solana/web3.js";
 
 export class FutarchyRPCProposalsClient implements FutarchyProposalsClient {
   private autocratProgram: Program<AutocratProgram>;
@@ -45,5 +46,49 @@ export class FutarchyRPCProposalsClient implements FutarchyProposalsClient {
         dao.treasury.toString()
       );
     });
+  }
+  async finalizeProposal(dao: PublicKey, proposal: PublicKey, daoTreasury: PublicKey): Promise<any> {
+    const storedProposal = await this.autocratProgram.account.proposal.fetch(proposal);
+    const expirySlot = storedProposal.slotEnqueued.toNumber() + 1_080_000; // TODO: This may be different
+    const current_slot = 0;
+    if (expirySlot < current_slot){
+      throw new Error('Unable to process request, the time period has not passed');
+    }
+
+    const accounts = storedProposal.instruction.accounts
+    const program = storedProposal.instruction.programId
+
+    try {
+      accounts[2].isSigner = false
+    } catch (e) {
+      console.error('Error:', e)
+    }
+
+    const _program = {
+      pubkey: program,
+      isSigner: false,
+      isWritable: false,
+    }
+
+    accounts.push(_program)
+
+    console.log(accounts)
+
+    let tx = await this.autocratProgram.methods
+      .finalizeProposal()
+      .accounts({
+        proposal,
+        openbookTwapPassMarket: storedProposal.openbookTwapPassMarket,
+        openbookTwapFailMarket: storedProposal.openbookTwapFailMarket,
+        dao,
+        baseVault: storedProposal.baseVault,
+        quoteVault: storedProposal.quoteVault,
+        vaultProgram: this.vaultProgram.programId,
+        daoTreasury,
+      })
+      .remainingAccounts(accounts)
+      .instruction()
+
+      return tx
   }
 }

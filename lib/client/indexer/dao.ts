063474c4c42aee04bb289f549dd14fa31c82348d
LukasDeco <41474751+LukasDeco@users.noreply.github.com>
2024-05-22 21:04:18 +0800

    feat: fetch token supply from indexer (#122)
    * feat: fetch token supply from indexer
    
    * chore: pckg json bump

--- lib/client/indexer/dao.ts
+++ lib/client/indexer/dao.ts
@@ -1,4 +1,10 @@
-import { Dao, DaoAccount, DaoAggregate, DaoDetails__GQL, FutarchyProtocol } from "@/types";
+import {
+  Dao,
+  DaoAccount,
+  DaoAggregate,
+  DaoDetails__GQL,
+  FutarchyProtocol
+} from "@/types";
 import { FutarchyDaoClient } from "@/client";
 import { FutarchyRPCDaoClient } from "../rpc";
 import { Client as IndexerGraphQLClient } from "./__generated__";
@@ -41,6 +47,7 @@ export class FutarchyIndexerDaoClient implements FutarchyDaoClient {
               mint_acct: true,
               name: true,
               image_url: true,
+              supply: true
             },
             tokenByQuoteAcct: {
               decimals: true,
@@ -48,17 +55,18 @@ export class FutarchyIndexerDaoClient implements FutarchyDaoClient {
               mint_acct: true,
               name: true,
               image_url: true,
+              supply: true
             },
             pass_threshold_bps: true,
             slots_per_proposal: true,
             treasury_acct: true,
             proposals_aggregate: {
               aggregate: {
-                count: true,
-              },
-            },
-          },
-        },
+                count: true
+              }
+            }
+          }
+        }
       });
 
       const daoAggregates = dao_details.map<DaoAggregate>((daoDetails) =>
@@ -78,9 +86,9 @@ export class FutarchyIndexerDaoClient implements FutarchyDaoClient {
         dao_details: {
           __args: {
             where: {
-              slug: { _eq: daoSlug },
+              slug: { _eq: daoSlug }
             },
-            limit: 1,
+            limit: 1
           },
           name: true,
           slug: true,
@@ -101,6 +109,7 @@ export class FutarchyIndexerDaoClient implements FutarchyDaoClient {
               mint_acct: true,
               name: true,
               image_url: true,
+              supply: true
             },
             tokenByQuoteAcct: {
               decimals: true,
@@ -108,17 +117,18 @@ export class FutarchyIndexerDaoClient implements FutarchyDaoClient {
               mint_acct: true,
               name: true,
               image_url: true,
+              supply: true
             },
             pass_threshold_bps: true,
             slots_per_proposal: true,
             treasury_acct: true,
             proposals_aggregate: {
               aggregate: {
-                count: true,
-              },
-            },
-          },
-        },
+                count: true
+              }
+            }
+          }
+        }
       });
       if (dao_details[0]) {
         return this.getDaoAggregateFromDaoDetailsQuery(dao_details[0]);
@@ -150,6 +160,7 @@ export class FutarchyIndexerDaoClient implements FutarchyDaoClient {
                 name: d.tokenByBaseAcct?.name ?? null,
                 publicKey: d.tokenByBaseAcct?.mint_acct,
                 url: d.tokenByBaseAcct?.image_url,
+                supply: d.tokenByBaseAcct?.supply ?? null
               },
               quoteToken: {
                 symbol: d.tokenByQuoteAcct?.symbol,
@@ -157,6 +168,7 @@ export class FutarchyIndexerDaoClient implements FutarchyDaoClient {
                 name: d.tokenByQuoteAcct?.name,
                 publicKey: d.tokenByQuoteAcct?.mint_acct,
                 url: d.tokenByQuoteAcct?.image_url,
+                supply: d.tokenByBaseAcct?.supply ?? null
               },
               daoAccount: {
                 // TODO these nullable conditions result in public keys that don't make sense potentially
@@ -167,22 +179,22 @@ export class FutarchyIndexerDaoClient implements FutarchyDaoClient {
                   : undefined,
                 proposalCount: d.proposals_aggregate.aggregate?.count,
                 passThresholdBps: d.pass_threshold_bps,
-                slotsPerProposal: d.slots_per_proposal,
+                slotsPerProposal: d.slots_per_proposal
               },
               publicKey: new PublicKey(d.dao_acct),
-              protocol: this.protocolMap.get(d.program_acct),
+              protocol: this.protocolMap.get(d.program_acct)
             };
           }
         })
-        .filter((d) => !!d) as Dao[],
+        .filter((d) => !!d) as Dao[]
     };
   }
 
-  async getMinLpToProvide(daoAggregate:DaoAggregate){
-    return this.rpcDaoClient.getMinLpToProvide(daoAggregate)
+  async getMinLpToProvide(daoAggregate: DaoAggregate) {
+    return this.rpcDaoClient.getMinLpToProvide(daoAggregate);
   }
 
-  async getTreasuryBalance(daoAccount: DaoAccount){
-    return this.rpcDaoClient.getTreasuryBalance(daoAccount)
+  async getTreasuryBalance(daoAccount: DaoAccount) {
+    return this.rpcDaoClient.getTreasuryBalance(daoAccount);
   }
 }

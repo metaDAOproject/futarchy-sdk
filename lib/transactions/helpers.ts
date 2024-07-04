f901dced16c2d1e5da07dac9bf5ff21a623ac5d4
LukasDeco <ldeconantsesznak@gmail.com>
2024-07-04 08:46:34 -0600

    fix: instanceof being a pill

---
+++ lib/transactions/helpers.ts
@@ -0,0 +1,52 @@
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import {
+  CompiledInstruction,
+  PublicKey,
+  TransactionInstruction
+} from "@solana/web3.js";
+
+export function convertTransactionInstruction(
+  instruction: TransactionInstruction,
+  accountKeys: PublicKey[]
+): CompiledInstruction {
+  const accountIndices = instruction.keys.map((keyObj) =>
+    accountKeys.findIndex((pubkey) => pubkey.equals(keyObj.pubkey))
+  );
+
+  if (accountIndices.includes(-1)) {
+    throw new Error("Account key not found in accountKeys list");
+  }
+
+  const programIdIndex = accountKeys.findIndex((pubkey) =>
+    pubkey.equals(instruction.programId)
+  );
+
+  if (programIdIndex === -1) {
+    throw new Error("Program ID not found in accountKeys list");
+  }
+
+  return {
+    programIdIndex,
+    accounts: accountIndices,
+    data: bs58.encode(instruction.data)
+  };
+}
+
+// Function to convert CompiledInstruction to Instruction
+export function convertCompiledInstruction(
+  compiledInstruction: CompiledInstruction,
+  programId: PublicKey,
+  accountKeys: PublicKey[]
+): TransactionInstruction {
+  const keys = compiledInstruction.accounts.map((index) => ({
+    pubkey: accountKeys[index],
+    isSigner: index === compiledInstruction.programIdIndex, // You might need to adjust this based on your logic
+    isWritable: true // This can be modified based on your requirements
+  }));
+
+  return new TransactionInstruction({
+    keys,
+    programId,
+    data: Buffer.from(compiledInstruction.data)
+  });
+}
